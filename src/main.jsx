01b31a2209db860c2a6c56cc2d0b6b98a049a4cc
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-03-24 09:22:14 +0100

    add theme palette

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import './styles/index.css'
+import './styles/palettes.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
