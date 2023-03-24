13d0fd8433059af4a26460180c114d64d539f323
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-03-24 09:00:53 +0100

    fix dropdown

--- src/components/CustomDropdown/CustomDropdown.jsx
+++ src/components/CustomDropdown/CustomDropdown.jsx
@@ -1,37 +1,41 @@
 import { Menu } from '@headlessui/react'
 import { Link } from 'react-router-dom'
 
+import styles from './styles.module.css'
+
 const navlinks = [
-  {to: '/', title: 'Home'},
-  {to: '/about', title: 'About'},
-  {to: '/contact', title: 'Contact'},
+  { to: '/', title: 'Home' },
+  { to: '/about', title: 'About' },
+  { to: '/contact', title: 'Contact' },
 ]
 
 function CustomDropdown() {
   return (
     <Menu>
-      <Menu.Button>Navigation</Menu.Button>
-      <Menu.Items>
+      <Menu.Button className={styles.button} >Navigation</Menu.Button>
+      <div className={styles.container}>
+        <Menu.Items className={styles.items}>
+          {
+            navlinks.map(
+              (link) => {
+                return (
+                  <Menu.Item key={link.title}>
+                    {({ active }) => (
+                      <Link
+                        to={link.to}
+                        className={styles.link}
+                      >
+                        {link.title}
+                      </Link>
+                    )}
+                  </Menu.Item>
+                )
+              }
+            )
+          }
 
-        {
-          navlinks.map(
-            (link) => {
-              return (
-                <Menu.Item>
-                  {({ active }) => (
-                    <Link
-                      to={link.to}
-                    >
-                      {link.title}
-                    </Link>
-                  )}
-                </Menu.Item>
-              )
-            }
-          )
-        }
-        
-      </Menu.Items>
+        </Menu.Items>
+      </div>
     </Menu>
   )
 }
